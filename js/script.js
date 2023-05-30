ff1a1702d788f49e480f35727137aef611e29562
tcunningham203 <tcunningham203@gmail.com>
2023-05-30 17:40:59 +0900

    css updates

--- js/script.js
+++ js/script.js
@@ -15,52 +15,46 @@ var tempSmall = document.getElementById("temp-small");
 var windSmall = document.getElementById("wind-small");
 var humSmall = document.getElementById("hum-small");
 
+var currentWeatherContainer = document.getElementById("weather-container");
+
 btnSearch.addEventListener("click", function (event) {
-    event.preventDefault(); // Prevent the default form submission behavior
-    console.log("Button clicked");
-    fetchData();
+  event.preventDefault();
+  fetchData();
 });
 
 function fetchData() {
-    var city = boxSearch.value;
-    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
+  var city = boxSearch.value;
+  var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`;
 
-    console.log("Fetching weather data...");
-
-    fetch(queryURL)
-        .then(function (response) {
-            if (response.ok) {
-                console.log("Weather data fetched successfully");
-                return response.json();
-            } else {
-                console.error("Error fetching current weather data: " + response.status);
-                throw new Error("Error fetching current weather data: " + response.status);
-            }
-        })
-        .then(function (data) {
-            console.log("Displaying current weather");
-            displayCurrentWeather(data);
-        })
-        .catch(function (error) {
-            console.error(error);
-        });
+  fetch(queryURL)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Error fetching current weather data: " + response.status);
+      }
+    })
+    .then(function (data) {
+      displayCurrentWeather(data);
+      currentWeatherContainer.classList.remove("hidden"); // Show the current weather container
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 }
 
 function displayCurrentWeather(data) {
-    // Extract the necessary information from the response data
-    var city = data.name;
-    var date = new Date(data.dt * 1000).toLocaleDateString();
-    var temperature = data.main.temp;
-    var windSpeed = data.wind.speed;
-    var humidity = data.main.humidity;
-    var iconCode = data.weather[0].icon;
-    var iconURL = `https://openweathermap.org/img/w/${iconCode}.png`;
+  var city = data.name;
+  var date = new Date(data.dt * 1000).toLocaleDateString();
+  var temperature = data.main.temp;
+  var windSpeed = data.wind.speed;
+  var humidity = data.main.humidity;
+  var iconCode = data.weather[0].icon;
+  var iconURL = `https://openweathermap.org/img/w/${iconCode}.png`;
 
-    // Update the DOM elements with the fetched weather data
-    console.log("Updating DOM with weather data");
-    dateBig.textContent = city + " (" + date + ")";
-    picBig.setAttribute("src", iconURL);
-    tempBig.textContent = "Temperature: " + temperature + " K";
-    windBig.textContent = "Wind: " + windSpeed + " m/s";
-    humBig.textContent = "Humidity: " + humidity + "%";
+  dateBig.textContent = city + " (" + date + ")";
+  picBig.setAttribute("src", iconURL);
+  tempBig.textContent = "Temperature: " + temperature + " K";
+  windBig.textContent = "Wind: " + windSpeed + " m/s";
+  humBig.textContent = "Humidity: " + humidity + "%";
 }
